1e513f6f3e8c8eb4750297f2447722a472e7cfc0
ShaliniJha1999 <46892946+ShaliniJha1999@users.noreply.github.com>
2019-10-21 20:22:07 +0530

    Update mergeSettings.js

--- PostDeployScripts/mergeSettings.js
+++ PostDeployScripts/mergeSettings.js
@@ -1,4 +1,4 @@
-const fs = require('fs');
+const fs = require('fs');  
 const path = require('path');
 const appSettingPath = path.resolve('./appsettings.json');
 const envPath = path.resolve('./.env');
